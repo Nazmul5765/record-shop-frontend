f1c0a41f8350e82b610e0847a0f5985cb07b2279
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-05-20 14:28:15 +0100

    Create Record model and display record details in RecordSummary component

---
+++ RecordShop.Web/RecordShop.Web/Models/Record.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordShop.Web.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Artist { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public int ReleaseYear { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
