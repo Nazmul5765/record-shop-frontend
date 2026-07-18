08c3f71969e654f1158e7bbaa3c08247364418a4
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-07-18 18:45:20 +0100

    feat: add record creation form

--- RecordShop.Web/RecordShop.Web/Models/Record.cs
+++ RecordShop.Web/RecordShop.Web/Models/Record.cs
@@ -3,6 +3,7 @@
     public class Record
     {
         public int Id { get; set; }
+        
         public string Title { get; set; } = string.Empty;
         public string Artist { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
